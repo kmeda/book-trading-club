fd1a11a63dabe201ad7c88d82945be6da86a8180
Karthik Meda <karthikreddymeda@gmail.com>
2017-08-20 19:16:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const axios = require("axios");
+const CircularJSON = require('circular-json');
+var favicon = require('serve-favicon');
+
+const app = express();
+app.use(favicon(path.join(__dirname + '/favicon.ico')));
+
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+
+import Home from './components/Home.jsx';
+
+import '../styles/main.scss';
+const actions = require('./actions/actions.jsx');
+var store = require('./store/configureStore.jsx').configure();
+
+ReactDOM.render(
+    <Provider store={store}>
+      <Home />
+    </Provider>,
+  document.getElementById('root'));
